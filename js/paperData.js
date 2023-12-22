40cfc305945d379eef9bdc7ab093de53b2dbbf51
wellido <qianghu0515@gamil.com>
2023-12-22 14:31:12 +0100

    update papers

--- js/paperData.js
+++ js/paperData.js
@@ -20,7 +20,9 @@ var paperArray = new Array();
 
 // Formal Specification for Deep Neural Networks
 
-paperArray.push(["data096", "2023", "Li Li, Chuanqi Tao, Hongjing Guo, Jingxuan Zhang, Xiaobing Sun", "FATS: Feature Distribution Analysis-Based Test Selection for Deep Learning Enhancement", "Sampling-Based Model Retraining"]);
+paperArray.push(["data097", "2023", "Muxi Chen, Yu Li, Qiang Xu", "HiBug: On Human-Interpretable Model Debug", "Retraining"]);
+
+paperArray.push(["data096", "2023", "Li Li, Chuanqi Tao, Hongjing Guo, Jingxuan Zhang, Xiaobing Sun", "FATS: Feature Distribution Analysis-Based Test Selection for Deep Learning Enhancement", "Retraining"]);
 
 paperArray.push(["data096", "2023", "Qinyuan Ye, Harvey Yiyun Fu, Xiang Ren, Robin Jia", "How Predictable Are Large Language Model Capabilities? A Case Study on BIG-bench", "Performance Estimation"]);
 
